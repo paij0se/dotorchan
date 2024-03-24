09c1252744a7f35c6999f14ec973e8d86c4c6da2
Jose Mejia <josemejia1928373@gmail.com>
2024-03-24 10:59:23 -0500

    check admin ip

--- lib/routes/hello.ex
+++ lib/routes/hello.ex
@@ -4,10 +4,16 @@ defmodule Dotorchan.Router do
   plug(:match)
   plug(:dispatch)
 
-  get "/" do
+  get "/admin" do
+    admin_ip = System.get_env("ADMIN_IP")
     ip = Tools.Ip.get_remote_ip(conn)
-    ip |> IO.inspect(label: "Remote IP")
-    send_resp(conn, 200, "Welcome!")
+    IO.inspect("#{ip == admin_ip}")
+    if ip == admin_ip do
+      send_resp(conn, 200, "Welcome!")
+    else
+      IO.inspect(admin_ip)
+      send_resp(conn, 403, "Forbidden")
+    end
   end
 
   match _ do
